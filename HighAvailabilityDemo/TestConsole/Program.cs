3c77f9522482fbf646089e23f6b06025379f1264
Massimo Bonanni <massimo.bonanni@tiscali.it>
2016-11-08 22:41:32 +0000

    Aggiornamento console di test

--- HighAvailabilityDemo/TestConsole/Program.cs
+++ HighAvailabilityDemo/TestConsole/Program.cs
@@ -9,24 +9,65 @@ namespace TestConsole
 {
     class Program
     {
+        private const string ApiUrlArg = "-apiurl:";
+        private const string SequenceNameArg = "-seqname:";
+        private const string NumOfIterationArg = "-numiter:";
+        private const string HelpArg = "-h";
+
+        private static string ApiUrl;
+        private static string SequenceName;
+        private static int NumOfIteration;
+
         static void Main(string[] args)
         {
-            string apiUrl = "http://localhost:9050/api/sequences/test";
-            if (args != null && args.Count() > 0)
+            if (args.Any(a => a.ToLower() == HelpArg))
             {
-                apiUrl = args[0];
+                WriteHelp();
+                return;
             }
 
-            for (int i = 0; i < 10000; i++)
+            RetrieveArguments(args);
+
+            string fullApiUrl = $"{ApiUrl}/api/sequences/{SequenceName }";
+            var client = new RestClient(fullApiUrl);
+
+            for (int i = 0; i < NumOfIteration; i++)
             {
-                var client = new RestClient(apiUrl);
                 var request = new RestRequest(Method.GET);
                 IRestResponse<GetNextSequenceResponse> response = client.Execute<GetNextSequenceResponse>(request);
 
                 Console.WriteLine($"[{response.Data.ActorId }] - Sequence {response.Data.Value } - Node {response.Data.NodeInfo }");
-                Task.Delay(100).GetAwaiter().GetResult();
-
+                Task.Delay(250).GetAwaiter().GetResult();
             }
+        }
+
+        private static void RetrieveArguments(string[] args)
+        {
+            var argString = args.FirstOrDefault(a => a.ToLower().StartsWith(ApiUrlArg));
+            if (argString != null)
+                ApiUrl = argString.ToLower().Replace(ApiUrlArg, "");
+            else
+                ApiUrl = "http://localhost:9050";
+
+            var argSplit = args.FirstOrDefault(a => a.ToLower().StartsWith(SequenceNameArg))?.Split(':');
+            if (argSplit != null && argSplit.Count() >= 2)
+                SequenceName = argSplit[1];
+            else
+                SequenceName = "test";
+
+            argSplit = args.FirstOrDefault(a => a.ToLower().StartsWith(NumOfIterationArg))?.Split(':');
+            NumOfIteration = 1000;
+            if (argSplit != null && argSplit.Count() >= 2)
+                int.TryParse(argSplit[1], out NumOfIteration);
+
+        }
+
+        private static void WriteHelp()
+        {
+            Console.WriteLine($"{ApiUrlArg}<url> indirizzo api (es. http://localhost:9050)");
+            Console.WriteLine($"{SequenceNameArg}<name> nome del sequence da testare");
+            Console.WriteLine($"{NumOfIterationArg}<num> numero di iterazioni (default 1000)");
+            Console.WriteLine();
         }
     }
 }
